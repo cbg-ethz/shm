c191efe6ebedc9d24c104bbf4d9ffe68c0dfa502
Simon Dirmeier <simon.dirmeier@web.de>
2019-04-04 10:48:15 +0200

    Traverses to python

---
+++ README.rst
@@ -0,0 +1,35 @@
+*****
+PyBDA
+*****
+
+A commandline tool for analysis of big biological data sets using Snakemake and Apache Spark.
+
+About
+=====
+
+PyBDA is a Python library and command line tool for big data analytics and machine learning scaling to tera byte sized data sets.
+
+In order to make PyBDA scale to big data sets, we use `Apache Spark`_'s DataFrame API which, if developed against, automatically distributes
+data to the nodes of a high-performance cluster and does the computation of expensive machine learning tasks in parallel.
+For scheduling, PyBDA uses Snakemake_ to automatically execute pipelines of jobs. In particular, PyBDA will first build a DAG of methods/jobs
+you want to execute in succession (e.g. dimensionality reduction into clustering) and then compute every method by traversing the DAG.
+In the case of a successful computation of a job, PyBDA will write results and plots, and create statistics. If one of the jobs fails PyBDA will report where and which method failed
+(owing to Snakemake's scheduling) such that the same pipeline can effortlessly be continued from where it failed the last time.
+
+Documentation
+=============
+
+Check out the documentation `here <https://pybda.readthedocs.io/en/latest/>`_.
+The documentation will walk you though
+
+* the installation process,
+* setting up Apache Spark,
+* using `pybda`.
+
+Author
+======
+
+Simon Dirmeier `simon.dirmeier at bsse.ethz.ch <mailto:simon.dirmeier@bsse.ethz.ch>`_.
+
+.. _`Apache Spark`: https://spark.apache.org/
+.. _Snakemake: https://snakemake.readthedocs.io/en/stable/
---
+++ setup.py
@@ -0,0 +1,73 @@
+from sys import version_info
+from setuptools import setup, find_packages
+
+if version_info[0] == 2:
+    exit("Sorry, Python 2 is not supported. Move to Python 3 already.")
+
+
+def readme():
+    with open('README.rst') as fl:
+        return fl.read()
+
+
+test_deps = [
+    'coverage',
+    'findspark',
+    'flake8',
+    'pylint',
+    'pytest>=3.6.2',
+    'pytest-cov',
+    'pytest-pep8',
+    'scikit-learn',
+    'yapf'
+]
+
+doc_deps = [
+    'sphinx',
+    'sphinx_fontawesome',
+    'sphinxcontrib-fulltoc'
+]
+
+setup(
+  name='pybda',
+  version='0.0.6',
+  description='Big biological data analysis with Snakemake and Apache Spark',
+  long_description=readme(),
+  url='https://github.com/cbg-ethz/pybda',
+  author='Simon Dirmeier',
+  author_email='simon.dirmeier@bsse.ethz.de',
+  license='GPLv3',
+  keywords='bigdata analysis pipeline workflow spark pyspark machinelearning',
+  packages=find_packages(),
+  scripts=['scripts/pybda'],
+  include_package_data=True,
+  python_requires='>=3',
+  install_requires=[
+      'click>=6.7',
+      'joypy>=0.1.9',
+      'matplotlib>=2.2.3',
+      'numpy>=1.15.0',
+      'pandas>=0.23.3',
+      'pyspark==2.4.0',
+      'scipy>=1.0.0',
+      'seaborn>=0.9.0',
+      'snakemake>=5.2.2',
+      'sparkhpc>=0.3.post4'
+  ],
+  test_requires=test_deps,
+  extras_require={
+      'test': test_deps,
+      'doc': doc_deps,
+      'dev': test_deps + doc_deps
+  },
+  classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Environment :: Console',
+      'Intended Audience :: End Users/Desktop',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6'
+  ]
+)

32a086b6ea3eccc7ce94e8acbffa19bf9d5a286a
Simon Dirmeier <simon.dirmeier@web.de>
2019-04-23 16:00:27 +0200

    Add proper posterior sampling for MRF

--- shm/distributions/BinaryMRF.py
+++ shm/distributions/BinaryMRF.py
@@ -27,6 +27,7 @@ class BinaryMRF(Discrete):
         self.mode = scipy.repeat(1, self.__n)
         self.__choice = scipy.stats.bernoulli.rvs()
         self.__point = scipy.stats.bernoulli.rvs(0.5, size=self.__n)
+        self.__blanket = {}
 
     @property
     def n_nodes(self):
@@ -36,10 +37,38 @@ class BinaryMRF(Discrete):
     def node_labels(self):
         return self.__node_labels
 
+    def random(self, point=None):
+        next_point = numpy.zeros(self.n_nodes)
+        for idx in range(self.node_labels):
+            next_point[idx] = self._gibbs(idx, point)
+        self.__point = next_point
+        return next_point
+
+    def posterior_sample(self, z, gamma, mu, tau):
+        logliks = self._loglik(gamma, mu, tau)
+        next_point = numpy.zeros(self.n_nodes)
+        for idx in range(self.node_labels):
+            next_point[idx] = self._gibbs(idx, z, logliks)
+        self.__point = next_point
+        return self.__point
+
+    def logp(self, value):
+        return 0
+
+    def _gibbs(self, idx, point, loglik=None):
+        edge_pot = self._log_edge_potential(point, idx)
+        node_pot = self._log_node_potential(idx, loglik) if loglik else 0
+        p = scipy.special.expit(2 * edge_pot - node_pot)
+        return self.__choice(p)
+
     def _markov_blank(self, idx):
+        if idx in self.__blanket:
+            return self.__blanket[idx]
         children = numpy.where(self.__adj[idx, :] != 0)[0]
         parents = numpy.where(self.__adj[:, idx] != 0)[0]
-        return numpy.unique(numpy.append(children, parents))
+        blanket = numpy.unique(numpy.append(children, parents))
+        self.__blanket[idx] = blanket
+        return blanket
 
     def _log_edge_potential(self, point, idx):
         mb = self._markov_blank(idx)
@@ -49,29 +78,8 @@ class BinaryMRF(Discrete):
         s2 = numpy.sum((blanket_labs != point_label) * mb_weights)
         return s1 - s2
 
-    def random(self, point=None):
-        next_point = numpy.zeros(self.n_nodes)
-        for idx in range(self.node_labels):
-            next_point[idx] = self._gibbs(idx, point)
-        self.__point = next_point
-        return next_point
-
-    def _gibbs(self, idx, point, loglik=None):
-        edge_pot = self._log_edge_potential(point, idx)
-        node_pot = self._node_potential(loglik) if loglik else 0
-        p = scipy.special.expit(2 * edge_pot - node_pot)
-        return self.__choice(p)
-
-    def logp(self, value):
-        return 0
-
-    def posterior_sample(self, z, gamma, mu, tau):
-        logliks = self._loglik(gamma, mu, tau)
-        next_point = numpy.zeros(self.n_nodes)
-        for idx in range(self.node_labels):
-            next_point[idx] = self._gibbs(idx, z, logliks)
-        self.__point = next_point
-        return self.__point
+    def _log_node_potential(self, idx, loglik):
+        return loglik[idx, 1] - loglik[idx, 0]
 
     def _loglik(self, gamma, mu, tau):
         neg = scipy.stats.norm.logpdf(gamma, mu[0], tau[0])

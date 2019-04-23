abca7a92717ad5696ee17102a5cfcb209960b3a4
Simon Dirmeier <simon.dirmeier@web.de>
2019-04-23 14:56:22 +0200

    Fixe edge potential computation (leave out point label)

--- shm/distributions/BinaryMRF.py
+++ shm/distributions/BinaryMRF.py
@@ -25,7 +25,7 @@ class BinaryMRF(Discrete):
         super(BinaryMRF, self).__init__(shape=self.__n, *args, **kwargs)
 
         self.mode = scipy.repeat(1, self.__n)
-        self.__choice = numpy.random.choice
+        self.__choice = scipy.stats.bernoulli.rvs()
         self.__point = scipy.stats.bernoulli.rvs(0.5, size=self.__n)
 
     @property
@@ -41,15 +41,13 @@ class BinaryMRF(Discrete):
         parents = numpy.where(self.__adj[:, idx] != 0)[0]
         return numpy.unique(numpy.append(children, parents))
 
-    def _edge_potential(self, point, idx):
+    def _log_edge_potential(self, point, idx):
         mb = self._markov_blank(idx)
         point_label, blanket_labs = point[idx], point[mb]
         mb_weights = self.__adj[mb, idx]
         s1 = numpy.sum((blanket_labs == point_label) * mb_weights)
         s2 = numpy.sum((blanket_labs != point_label) * mb_weights)
-        # TODO: is point_label * required (llokn up)
-        # i think i can actually leave it out
-        return point_label * (s1 - s2)
+        return s1 - s2
 
     def random(self, point=None):
         next_point = numpy.zeros(self.n_nodes)
@@ -58,9 +56,11 @@ class BinaryMRF(Discrete):
         self.__point = next_point
         return next_point
 
-    def _gibbs(self, idx, point):
-        p = scipy.special.expit(self._edge_potential(point, idx))
-        return self.__choice([-1, 1], p=[p, 1 - p])
+    def _gibbs(self, idx, point, loglik=None):
+        edge_pot = self._log_edge_potential(point, idx)
+        node_pot = self._node_potential(loglik) if loglik else 0
+        p = scipy.special.expit(2 * edge_pot - node_pot)
+        return self.__choice(p)
 
     def logp(self, value):
         return 0
@@ -69,7 +69,7 @@ class BinaryMRF(Discrete):
         logliks = self._loglik(gamma, mu, tau)
         next_point = numpy.zeros(self.n_nodes)
         for idx in range(self.node_labels):
-            next_point[idx] = self._gibbs(idx, z)
+            next_point[idx] = self._gibbs(idx, z, logliks)
         self.__point = next_point
         return self.__point
 
@@ -79,7 +79,5 @@ class BinaryMRF(Discrete):
         return scipy.column_stack((neg, pos))
 
     def _repr_latex_(self, name=None, dist=None):
-        if dist is None:
-            dist = self
         name = r'\text{%s}' % name
         return r'${} \sim \text{{BinaryMRF}}(\dots)$'.format(name)

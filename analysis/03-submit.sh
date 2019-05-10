37eeeeccffc2f007a019edb48cc2542621126253
Simon Dirmeier <simon.dirmeier@web.de>
2019-05-11 00:28:16 +0200

    Update script

--- analysis/03-submit.sh
+++ analysis/03-submit.sh
@@ -3,62 +3,32 @@
 
 function test_run {
 	echo "Running tests"
-	bsub -W 4:00 -n 1 -R "rusage[mem=15000]" python run_shm.py sample \
-	    ../data_raw/easy_simulated_data/large-simulated_data.tsv \
-	    /cluster/home/simondi/simondi/data/shm/test-mrf_model \
-	    --family gaussian \
-	    --ntune 100 \
-	    --ndraw 100 \
-	    --model mrf \
-	    --graph ../data_raw/easy_simulated_data/large-graph.tsv
-
-	bsub -W 4:00 -n 1 -R "rusage[mem=15000]" python run_shm.py sample \
-	    ../data_raw/easy_simulated_data/large-simulated_data.tsv \
-	    /cluster/home/simondi/simondi/data/shm/test-clustering_model \
-	    --family gaussian \
-	    --ntune 100 \
-	    --ndraw 100 \
-	    --model clustering \
-	    --graph ../data_raw/easy_simulated_data/large-graph.tsv
-
-	bsub -W 4:00 -n 1 -R "rusage[mem=15000]" python run_shm.py sample \
-	    ../data_raw/easy_simulated_data/large-simulated_data.tsv \
-	    /cluster/home/simondi/simondi/data/shm/test-simple_model \
-	    --family gaussian \
-	    --ntune 100 \
-	    --ndraw 100 \
-	    --model simple \
-	    --graph ../data_raw/easy_simulated_data/large-graph.tsv
+	for i in "simple" "mrf" "clustering";
+    do
+        bsub -W 4:00 -n 1 -R "rusage[mem=15000]" python run_shm.py sample \
+	        ../data_raw/simulated_data.tsv \
+            "/cluster/home/simondi/simondi/data/shm/test-${i}_model" \
+            --family gaussian \
+            --ntune 100 \
+            --ndraw 100 \
+            --model ${i} \
+            --graph ../data_raw/graph.pickle
+    done
 }
 
 function submit_run {
 	echo "Submitting jobs"
-	bsub -W 24:00 -n 1 -R "rusage[mem=25000]" python run_shm.py sample \
-	    ../data_raw/easy_simulated_data/large-simulated_data.tsv \
-	    /cluster/home/simondi/simondi/data/shm/large-mrf_model \
-	    --family gaussian \
-	    --ntune 100000 \
-	    --ndraw 20000 \
-	    --model mrf \
-	    --graph ../data_raw/easy_simulated_data/large-graph.tsv
-
-	bsub -W 24:00 -n 1 -R "rusage[mem=25000]" python run_shm.py sample \
-	    ../data_raw/easy_simulated_data/large-simulated_data.tsv \
-	    /cluster/home/simondi/simondi/data/shm/large-clustering_model \
-	    --family gaussian \
-	    --ntune 100000 \
-	    --ndraw 20000 \
-	    --model clustering \
-	    --graph ../data_raw/easy_simulated_data/large-graph.tsv
-
-	bsub -W 24:00 -n 1 -R "rusage[mem=25000]" python run_shm.py sample \
-	    ../data_raw/easy_simulated_data/large-simulated_data.tsv \
-	    /cluster/home/simondi/simondi/data/shm/large-simple_model \
-	    --family gaussian \
-	    --ntune 100000 \
-	    --ndraw 20000 \
-	    --model simple \
-	    --graph ../data_raw/easy_simulated_data/large-graph.tsv
+	for i in "simple" "mrf" "clustering";
+    do
+        bsub -W 24:00 -n 1 -R "rusage[mem=25000]" python run_shm.py sample \
+	        ../data_raw/simulated_data.tsv \
+            "/cluster/home/simondi/simondi/data/shm/${i}_model" \
+            --family gaussian \
+            --ntune 100000 \
+            --ndraw 20000 \
+            --model ${i} \
+            --graph ../data_raw/graph.pickle
+    done
 }
 
 

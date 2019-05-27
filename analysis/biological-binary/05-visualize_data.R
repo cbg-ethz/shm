5e4d2d376595e7ba097ff569d430b92325a54438
Simon Dirmeier <simon.dirmeier@web.de>
2019-05-27 22:10:02 +0200

    Add plot script and compare model script

---
+++ analysis/biological-binary/05-visualize_data.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(gridExtra)
+library(grid)
+library(ggplot2)
+
+dt <- readr::read_tsv("../../data_raw/biological_binary-data.tsv")
+graph <- readr::read_tsv("../../data_raw/biological_binary-graph.tsv")
+
+tt <- ttheme_minimal(base_size=12,
+                     base_family="Arial Narrow",
+                     core=list(
+                       bg_params = list(fill="white")))
+
+
+
+dt <- dt %>%
+  dplyr::select(gene, condition, intervention, replicate, copynumber, readout)
+dt <- dt[1:10, ]
+dt[10,] <- "..."
+
+g <- tableGrob(as.data.frame(dt), theme=tt)
+ggsave(g,
+       filename="~/personal/Sci/phd/presentations/2019_05_29_group_meeting/fig/data_raw.svg",
+       width=7.5, height=3, dpi=1000,)
+
+s <- as.tibble(s) %>%
+  tidyr::gather(sample, value)
--- analysis/simulated-binary/03-plot_traces.py
+++ analysis/simulated-binary/03-plot_traces.py
@@ -43,8 +43,7 @@ def _plot_network(graph, data, out_dir):
     networkx.draw_networkx_nodes(
       data["graph"].subgraph(["PSMB1", "POLR1B"]), pos=pos,
       nodelist=list(["PSMB1", "POLR1B"]), node_size=300,
-      node_color='#E84646', font_size=15, alpha=.9,
-      label="No")
+      node_color='black', font_size=15, alpha=.9)
     networkx.draw_networkx_nodes(
       data["graph"], pos=pos,
       nodelist=list(data['nonessential_genes']), node_size=300,
@@ -140,16 +139,16 @@ def plot_model(graph, data, readout, trace, ppc_trace,
     _write_params(model, data, trace, out_dir)
     print("network")
     _plot_network(graph, data, out_dir)
-    print("data")
-    _plot_data(readout, ppc_trace, out_dir)
-    print("trace")
-    _plot_trace(trace, model, out_dir)
-    print("hist")
-    _plot_hist(trace, model, out_dir)
-    print("forest")
-    _plot_forest(trace, data, model, out_dir)
-    print("labels")
-    _plot_posterior_labels(trace, model, out_dir)
+    # print("data")
+    # _plot_data(readout, ppc_trace, out_dir)
+    # print("trace")
+    # _plot_trace(trace, model, out_dir)
+    # print("hist")
+    # _plot_hist(trace, model, out_dir)
+    # print("forest")
+    # _plot_forest(trace, data, model, out_dir)
+    # print("labels")
+    # _plot_posterior_labels(trace, model, out_dir)
 
 
 @click.command()
@@ -168,11 +167,11 @@ def run(trace, readout_file, graph_file, pickl_file, model_type):
 
     with HLM(readout, model=model_type, graph=graph) as model:
         trace = pm.load_trace(trace, model=model.model)
-        ppc_trace = pm.sample_posterior_predictive(trace, 10000, model.model)
+        #ppc_trace = pm.sample_posterior_predictive(trace, 10000, model.model)
 
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
-    plot_model(graph, data, readout, trace, ppc_trace,
+    plot_model(graph, data, readout, trace, None,
                trace, model, out_dir)
 
 

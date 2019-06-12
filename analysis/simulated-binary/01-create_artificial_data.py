051b25dc0ac55244cf7b8fe4de0164686c5edc32
dirmeier <simon.dirmeier@bsse.ethz.ch>
2019-06-12 14:01:32 +0200

    Refactored data generation

--- analysis/simulated-binary/01-create_artificial_data.py
+++ analysis/simulated-binary/01-create_artificial_data.py
@@ -11,11 +11,12 @@ import scipy as sp
 import scipy.stats as st
 
 outpath = os.path.join("..", "data_raw")
-gamma_tau = .1
-beta_tau = .1
-l_tau = .1
-data_tau = .1
-gamma_tau_non_essential = .1
+
+gamma_tau = .25
+beta_tau = .25
+l_tau = .25
+data_tau = .25
+gamma_tau_non_essential = .25
 n_conditions, n_sgrnas, n_replicates = 2, 5, 5
 
 
@@ -25,7 +26,8 @@ def read_graph(infile):
     return G
 
 
-def get_gamma(n_essential, n_nonessential, gamma_tau, gamma_tau_non_essential):
+def get_gamma(n_essential, n_nonessential,
+              gamma_tau, gamma_tau_non_essential):
     np.random.seed(1)
     gamma_essential = sp.random.normal(-1, scale=gamma_tau, size=n_essential)
     gamma_nonessential = sp.random.normal(0, scale=gamma_tau_non_essential,
@@ -34,19 +36,18 @@ def get_gamma(n_essential, n_nonessential, gamma_tau, gamma_tau_non_essential):
     return gamma, gamma_essential, gamma_nonessential
 
 
-def write_file(G, G_filtered, genes, gamma_essential, gamma_nonessential,
+def write_file(G, genes, gamma_essential, gamma_nonessential,
                gamma, beta, l, count_table, suffix):
     count_table.to_csv(
-      os.path.join(outpath, "{}-simulated_data.tsv".format(suffix)),
+      os.path.join(outpath, "simulated_binary-{}-simulated_data.tsv".format(suffix)),
       index=False, sep="\t")
 
     with open(
-      os.path.join(outpath, "{}-graph.pickle".format(suffix)), "wb") as out:
-        pickle.dump(G_filtered.subgraph(genes), out)
+      os.path.join(outpath, "simulated_binary-{}-graph.pickle".format(suffix)), "wb") as out:
+        pickle.dump(G.subgraph(genes), out)
 
     data = {
         "graph": G.subgraph(genes),
-        "graph_used": G_filtered.subgraph(genes),
         "genes": genes,
         "essential_genes": genes[:len(gamma_essential)],
         "nonessential_genes": genes[len(gamma_essential):],
@@ -62,13 +63,37 @@ def write_file(G, G_filtered, genes, gamma_essential, gamma_nonessential,
         "data_tau": data_tau,
         "count_table": count_table
     }
-    picklepath = os.path.join(outpath, "{}-data.pickle".format(suffix))
+    picklepath = os.path.join(outpath, "simulated_binary-{}-data.pickle".format(suffix))
     with open(picklepath, "wb") as out:
         pickle.dump(data, out)
 
 
-def build_data(G, G_filtered, essential_genes, nonessential_genes,
-               suffix, with_interventions):
+def _build_data(idx, count_table, with_interventions, l, o, G,
+                genes, gamma_essential, gamma_nonessential, gamma, beta):
+    count_table = count_table.copy()
+    polr1b_idx = np.where(count_table['gene'] == 'POLR1B')[0][:idx * n_replicates]
+    psmb1_idx = np.where(count_table['gene'] == 'PSMB1')[0][:idx * n_replicates]
+    if not with_interventions:
+        l[:] = 0
+        o[:] = 1
+
+    count_table["affinity"] = o[count_table["intervention"]]
+    count_table["affinity"][polr1b_idx] = .1
+    count_table["affinity"][psmb1_idx] = .1
+    count_table["l"] = l[count_table["intervention"]]
+
+    count_table["readout"] = st.norm.rvs(
+      count_table["l"] +
+      count_table["affinity"] * (count_table["beta"] + count_table["gamma"]),
+      data_tau)
+
+    count_table["intervention"] = \
+        ["S" + str(i) for i in count_table["intervention"]]
+    write_file(G, genes, gamma_essential, gamma_nonessential,
+               gamma, beta, l, count_table, "{}_modified_grnas".format(idx))
+
+
+def build_data(G, essential_genes, nonessential_genes, with_interventions):
     genes = np.append(essential_genes, nonessential_genes)
     n_genes = len(genes)
     n_essential = len(essential_genes)
@@ -104,85 +129,9 @@ def build_data(G, G_filtered, essential_genes, nonessential_genes,
     l = st.norm.rvs(0, l_tau, size=n_conditions * n_genes * n_sgrnas)
     o = st.uniform.rvs(0, 1, size=n_conditions * n_genes * n_sgrnas)
 
-    #
-    # polr1b_idx = np.where(count_table['gene'] == 'POLR1B')[0]#[:10]
-    # psmb1_idx = np.where(count_table['gene'] == 'PSMB1')[0]#[:10]
-    # if not with_interventions:
-    #     l[:] = 0
-    #     o[:] = 1
-    #
-    # count_table['gamma'][polr1b_idx] = 0
-    # count_table['gamma'][psmb1_idx] = 0
-    #
-    # count_table["affinity"] = o[count_table["intervention"]]
-    # count_table["affinity"][polr1b_idx] = .1
-    # count_table["affinity"][psmb1_idx] = .1
-    # count_table["l"] = l[count_table["intervention"]]
-    #
-    # count_table["readout"] = st.norm.rvs(
-    #   count_table["l"] +
-    #   count_table["affinity"] * (count_table["beta"] + count_table["gamma"]),
-    #   data_tau)
-    #
-    # count_table["intervention"] = ["S" + str(i) for i in
-    #                                count_table["intervention"]]
-    # write_file(G, G_filtered, genes, gamma_essential, gamma_nonessential,
-    #            gamma, beta, l,
-    #            count_table, "two_genes_zero")
-
-    # polr1b_idx = np.where(count_table['gene'] == 'POLR1B')[0][:10]
-    # psmb1_idx = np.where(count_table['gene'] == 'PSMB1')[0][:10]
-    # if not with_interventions:
-    #     l[:] = 0
-    #     o[:] = 1
-    #
-    # count_table["affinity"] = o[count_table["intervention"]]
-    # count_table["affinity"][polr1b_idx] = .1
-    # count_table["affinity"][psmb1_idx] = .1
-    # count_table["l"] = l[count_table["intervention"]]
-    #
-    # count_table["readout"] = st.norm.rvs(
-    #   count_table["l"] +
-    #   count_table["affinity"] * (count_table["beta"] + count_table["gamma"]),
-    #   data_tau)
-    #
-    # count_table["intervention"] = ["S" + str(i) for i in
-    #                                count_table["intervention"]]
-    # write_file(G, G_filtered, genes, gamma_essential, gamma_nonessential,
-    #            gamma, beta, l,
-    #            count_table, "two_bad_sgrnas")
-
-    n_bad = 7
-    polr1b_idx = np.where(count_table['gene'] == 'POLR1B')[0][:n_replicates * n_bad]
-    psmb1_idx = np.where(count_table['gene'] == 'PSMB1')[0][:n_replicates * n_bad]
-    if not with_interventions:
-        l[:] = 0
-        o[:] = 1
-
-    count_table["affinity"] = o[count_table["intervention"]]
-    count_table["affinity"][polr1b_idx] = .1
-    count_table["affinity"][psmb1_idx] = .1
-    count_table["l"] = l[count_table["intervention"]]
-
-    count_table["readout"] = st.norm.rvs(
-      count_table["l"] +
-      count_table["affinity"] * (count_table["beta"] + count_table["gamma"]),
-      data_tau)
-
-    count_table["intervention"] = ["S" + str(i) for i in
-                                   count_table["intervention"]]
-    write_file(G, G_filtered, genes, gamma_essential, gamma_nonessential,
-               gamma, beta, l,
-               count_table, "{}_bad_sgrnas".format(n_bad))
-
-
-def filtered_graph(G, essential_genes, nonessential_genes):
-    A = networkx.subgraph(G, essential_genes)
-    B = networkx.subgraph(G, nonessential_genes)
-    G = networkx.Graph()
-    G.add_edges_from(list(A.edges()) + list(B.edges()))
-    G.add_nodes_from(list(A.nodes()) + list(B.nodes()))
-    return G
+    for idx in [2, 5, 7, 10]:
+        _build_data(idx, count_table, with_interventions, l, o, G,
+                    genes, gamma_essential, gamma_nonessential, gamma, beta)
 
 
 @click.command()
@@ -192,7 +141,7 @@ def run(size, with_interventions):
     essential_genes = sorted(["POLR2C", "POLR1B", "POLR2D", 'POLR3K',
                               "PSMC1", "PSMD4", 'PSMC5', 'PSMB1', 'PSMC3'])
 
-    G = read_graph("../data_raw/full_graph.pickle")
+    G = read_graph("../data_raw/simulated_binary-full_graph.pickle")
     nonessential_genes = np.setdiff1d(G.nodes(), essential_genes)
     np.random.seed(23)
     nonessential_genes = list(np.random.choice(nonessential_genes, 21))
@@ -205,13 +154,9 @@ def run(size, with_interventions):
         G.add_edge('PSMB1', 'POLR1B')
     filter_genes = np.sort(np.append(essential_genes, nonessential_genes))
     G = G.subgraph(np.sort(filter_genes))
-    G_filtered = filtered_graph(G, essential_genes, nonessential_genes)
 
-    if size == "small":
-        G_filtered = G
-    build_data(G, G_filtered,
-               essential_genes, nonessential_genes,
-               "", with_interventions=False)
+    build_data(G, essential_genes, nonessential_genes,
+               with_interventions=with_interventions)
 
 
 if __name__ == "__main__":

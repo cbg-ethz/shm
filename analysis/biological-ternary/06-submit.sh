d0175d1e1e4c23b1b436a6c5ff2ab005946e7b3e
Simon Dirmeier <simon.dirmeier@web.de>
2019-07-02 16:19:15 +0200

    Update ternary script

--- analysis/biological-ternary/06-submit.sh
+++ analysis/biological-ternary/06-submit.sh
@@ -8,7 +8,7 @@ function submit_run {
         bsub -W 24:00 -n 1 -R "rusage[mem=25000]" python run_shm.py \
             ../../data_raw/biological_ternary-data.tsv \
              ../../data_raw/biological_ternary-graph.pickle \
-            "/cluster/home/simondi/simondi/data/shm/biological_binary-${i}_model" \
+            "/cluster/home/simondi/simondi/data/shm/biological_ternary-${i}_model" \
             --family gaussian \
             --ntune 20000 \
             --ndraw 30000 \

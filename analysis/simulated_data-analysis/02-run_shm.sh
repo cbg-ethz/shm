4c79b0486b63b944967b5442f95a2bdc5bf2d3e9
dirmeier <simon.dirmeier@web.de>
2019-07-07 15:04:10 +0200

    Fixes paths

--- analysis/simulated_data-analysis/02-run_shm.sh
+++ analysis/simulated_data-analysis/02-run_shm.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 function submit_run {
-	echo "Submitting jobs"
-	for i in "mrf" "clustering";
+    echo "Submitting jobs"
+    for i in "mrf" "clustering";
     do
         for j in "0_modified_grnas" "2_modified_grnas" "7_modified_grnas" "10_modified_grnas"
         do
-            for k in "noise_sd_0.1" "noise_sd_0.2" "noise_sd_0.5" "noise_sd_1"
+            for k in "noise_sd_low" "noise_sd_middle" "noise_sd_high"
             do
                 for f in {1..10}
                 do
-                    echo -W 4:00 -n 1 -R "rusage[mem=25000]" python 02-run_shm.py \
+                    bsub -W 4:00 -n 1 -R "rusage[mem=25000]" python 02-run_shm.py \
                         ${f} \
                         ../../data_raw/simulated-large-${j}-${k}-simulated_data.tsv \
-                        "/cluster/home/simondi/simondi/data/shm/simulated_data/${i}_model-${j}-${k}-fold_${f}" \
                         ../../data_raw/simulated-large-${j}-${k}-graph.pickle \
-                        --ntune 20000 \
+                        "/cluster/home/simondi/simondi/data/shm/simulated/simulated_data-${i}_model-${j}-${k}-fold_${f}" \
+                        --ntune 30000 \
                         --ndraw 10000 \
                         --model ${i}
                 done
